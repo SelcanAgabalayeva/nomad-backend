041bca0e8975a48c9f65757e41bee708999b9a51
SelcanAgabalayeva <selcanagabalayeva12@gmail.com>
2026-07-22 19:29:48 +0400

    prepare render deployment

--- src/main/java/nomad/example/nomad_backend/config/GoogleSheetsConfig.java
+++ src/main/java/nomad/example/nomad_backend/config/GoogleSheetsConfig.java
@@ -8,27 +8,50 @@ import com.google.api.services.sheets.v4.SheetsScopes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 @Configuration
 public class GoogleSheetsConfig {
 
-    @Value("${google.credentials.path}")
-    private Resource credentialsResource;
+    @Value("${GOOGLE_CREDENTIALS:}")
+    private String googleCredentials;
+
 
     @Bean
     public Sheets sheets() throws Exception {
 
+        InputStream inputStream;
+
+        if (googleCredentials != null && !googleCredentials.isBlank()) {
+
+            inputStream = new ByteArrayInputStream(
+                    googleCredentials.getBytes()
+            );
+
+        } else {
+
+            inputStream = new ClassPathResource(
+                    "google-credentials.json"
+            ).getInputStream();
+
+        }
+
+
         GoogleCredential credential = GoogleCredential
-                .fromStream(credentialsResource.getInputStream())
+                .fromStream(inputStream)
                 .createScoped(List.of(SheetsScopes.SPREADSHEETS));
+
 
         return new Sheets.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 GsonFactory.getDefaultInstance(),
                 credential
-        ).setApplicationName("nomad-backend").build();
+        )
+                .setApplicationName("nomad-backend")
+                .build();
     }
 }

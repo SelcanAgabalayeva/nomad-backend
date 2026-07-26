f76cf0b261d93415affb17c5ade04fa23b27977f
SelcanAgabalayeva <selcanagabalayeva12@gmail.com>
2026-07-26 13:58:16 +0400

    Db problem

--- src/main/java/nomad/example/nomad_backend/controller/OpportunityController.java
+++ src/main/java/nomad/example/nomad_backend/controller/OpportunityController.java
@@ -23,7 +23,7 @@ public class OpportunityController {
     private final OpportunityRepository repository;
     private final ProjectService projectService;
     // məlumatını qarışdırır) - bu, ayrıca diqqət tələb edən mövcud bir
-    // məsələdir, bu dəyişikliklə əlaqəli deyl.
+    // məsələdir, bu dəyişikliklə əlaqəli deyiil.
     @GetMapping
     public List<Opportunity> getAll() {
         return repository.findAll();

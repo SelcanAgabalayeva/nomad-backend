3e3a8d9136b8873cb0f44164edbfaa2144df106e
SelcanAgabalayeva <selcanagabalayeva12@gmail.com>
2026-07-30 16:37:19 +0400

    kasksk

--- src/main/java/nomad/example/nomad_backend/repository/OpportunityRepository.java
+++ src/main/java/nomad/example/nomad_backend/repository/OpportunityRepository.java
@@ -18,19 +18,18 @@ public interface OpportunityRepository extends JpaRepository<Opportunity, Long> 
     Optional<Opportunity> findByUniqueKey(String uniqueKey);
 
     @Query("""
-SELECT o FROM Opportunity o 
-WHERE 
-o.active = true
+SELECT o FROM Opportunity o
+WHERE o.active = true
 AND (:category IS NULL OR o.category = :category)
 AND (
     :format IS NULL
-    OR LOWER(o.typeDetail) = LOWER(:format)
+    OR LOWER(o.typeDetail) LIKE CONCAT('%', LOWER(CAST(:format AS string)), '%')
 )
 AND (
     :search IS NULL
-    OR LOWER(o.title) LIKE LOWER(CONCAT('%', :search, '%'))
-    OR LOWER(o.country) LIKE LOWER(CONCAT('%', :search, '%'))
-    OR LOWER(o.typeDetail) LIKE LOWER(CONCAT('%', :search, '%'))
+    OR LOWER(o.title) LIKE CONCAT('%', LOWER(CAST(:search AS string)), '%')
+    OR LOWER(o.country) LIKE CONCAT('%', LOWER(CAST(:search AS string)), '%')
+    OR LOWER(o.typeDetail) LIKE CONCAT('%', LOWER(CAST(:search AS string)), '%')
 )
 """)
     List<Opportunity> searchOpportunities(

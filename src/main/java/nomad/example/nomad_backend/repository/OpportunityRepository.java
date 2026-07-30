2596186e3144112ea93979d8d46da9624ae06954
SelcanAgabalayeva <selcanagabalayeva12@gmail.com>
2026-07-30 15:27:53 +0400

    Add new

--- src/main/java/nomad/example/nomad_backend/repository/OpportunityRepository.java
+++ src/main/java/nomad/example/nomad_backend/repository/OpportunityRepository.java
@@ -20,6 +20,9 @@ public interface OpportunityRepository extends JpaRepository<Opportunity, Long> 
     @Query("SELECT o FROM Opportunity o WHERE " +
             "o.active = true AND " +
             "(:category IS NULL OR o.category = :category) AND " +
+            "(:format IS NULL OR LOWER(o.typeDetail) LIKE LOWER(CONCAT('%', :format, '%')) OR " + // <-- LIKE ƏLAVƏ EDİLDİ
+            "(:format = 'Onlayn' AND LOWER(o.typeDetail) LIKE '%online%') OR " + // <-- "Onlayn" gələrsə "online" da axtarır
+            "(:format = 'Əyani' AND (LOWER(o.typeDetail) LIKE '%offline%' OR LOWER(o.typeDetail) LIKE '%əyani%'))) AND " +
             "(:search IS NULL OR " +
             "LOWER(o.title) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
             "LOWER(o.country) LIKE LOWER(CONCAT('%', :search, '%')) OR " +

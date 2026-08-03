bdc9037b9c6cc836c526c4da974d519ca9b2fc12
SelcanAgabalayeva <selcanagabalayeva12@gmail.com>
2026-08-03 15:03:00 +0400

    Add Cloudinary profile image upload

---
+++ src/main/java/nomad/example/nomad_backend/config/CloudinaryConfig.java
@@ -0,0 +1,28 @@
+package nomad.example.nomad_backend.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import nomad.example.nomad_backend.entity.CloudinaryProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import lombok.RequiredArgsConstructor;
+@Configuration
+@RequiredArgsConstructor
+public class CloudinaryConfig {
+
+
+    private final CloudinaryProperties properties;
+
+
+    @Bean
+    public Cloudinary cloudinary(){
+
+        return new Cloudinary(
+                ObjectUtils.asMap(
+                        "cloud_name", properties.getCloudName(),
+                        "api_key", properties.getApiKey(),
+                        "api_secret", properties.getApiSecret()
+                )
+        );
+    }
+}
--- src/main/java/nomad/example/nomad_backend/service/impls/FileStorageService.java
+++ src/main/java/nomad/example/nomad_backend/service/impls/FileStorageService.java
@@ -1,50 +1,33 @@
 package nomad.example.nomad_backend.service.impls;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.UUID;
-
-import java.io.IOException;
-import java.nio.file.*;
-import java.util.UUID;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class FileStorageService {
 
-
-    private final String uploadDir = "uploads/profile/";
+    private final Cloudinary cloudinary;
 
 
     public String save(MultipartFile file) throws IOException {
 
-
-        String filename =
-                UUID.randomUUID()
-                        + "_"
-                        + file.getOriginalFilename();
-
-
-        Path filePath =
-                Paths.get(uploadDir + filename);
+        Map uploadResult =
+                cloudinary.uploader()
+                        .upload(
+                                file.getBytes(),
+                                ObjectUtils.emptyMap()
+                        );
 
 
-        Files.createDirectories(
-                filePath.getParent()
-        );
-
-
-        Files.copy(
-                file.getInputStream(),
-                filePath,
-                StandardCopyOption.REPLACE_EXISTING
-        );
-
-
-        return "/uploads/profile/" + filename;
+        return uploadResult
+                .get("secure_url")
+                .toString();
     }
 }
